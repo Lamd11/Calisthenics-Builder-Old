6a57661411233d56bceab0809b4a9cfd450db5d7
Daniel <daniel.lam3987@gmail.com>
2024-07-26 15:20:22 -0400

    Editted Progressions CSS. skills.json is intialized, adding skills to file. progressions.js will use fetch API

---
+++ javascript/progressions.js
@@ -0,0 +1,10 @@
+const skillCards = document.getElementById("skill-cards");
+const skillDropdownList = document.getElementById("filter");
+
+const skills = [
+    {
+        name: "Planche"
+        
+    },
+
+]
